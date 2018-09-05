e3b88e2abbe7bc95d3b4e155c002824871c66783
Verdorama <42942529+Verdorama@users.noreply.github.com>
2018-09-05 15:41:22 -0600

    Add files via upload

---
+++ eDoc/CR/eCreditNote.php
@@ -0,0 +1,15 @@
+<?php
+/*	$Id: eCreditNote.php $ */
+/*	Script to generate the credt note XML file. */
+/*	This program is under the GNU General Public License version 3. Verdorama 2017-11-10. */
+
+// BEGIN: Functions division ===================================================
+// END: Functions division =====================================================
+
+// BEGIN: Data division ========================================================
+// END: Data division ==========================================================
+
+// BEGIN: Procedure division ===================================================
+// END: Procedure division =====================================================
+
+?>
---
+++ eDoc/CR/eDebitNote.php
@@ -0,0 +1,15 @@
+<?php
+/*	$Id: eDebitNote.php $ */
+/*	Script to generate the debit note XML file. */
+/*	This program is under the GNU General Public License version 3. Verdorama 2017-11-10. */
+
+// BEGIN: Functions division ===================================================
+// END: Functions division =====================================================
+
+// BEGIN: Data division ========================================================
+// END: Data division ==========================================================
+
+// BEGIN: Procedure division ===================================================
+// END: Procedure division =====================================================
+
+?>
